afb5feb42a1d6142acfb3f9b70dadf125b35ae70
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-02-25 09:13:00 -0800

    Update player.gd

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ var impatient_on = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	$CollisionShape2D.disabled = false
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
