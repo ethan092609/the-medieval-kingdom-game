1f9fbca143d317079274cbebb79476ecb1fa3e37
LuckyJames128 <166640136+LuckyJames128@users.noreply.github.com>
2025-03-12 10:01:03 -0700

    Update main.gd to match main

--- main.gd
+++ main.gd
@@ -23,19 +23,8 @@ func _process(_delta):
 		$TileMap/NPC_Viking_2/AnimatedSprite2D.flip_h = true
 	else:
 		$TileMap/NPC_Viking_2/AnimatedSprite2D.flip_h = false
-	
-	if $TileMap/player.position.x < $TileMap/NPC_Viking_3.position.x:
-		$TileMap/NPC_Viking_3/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Viking_3/AnimatedSprite2D.flip_h = false
-	
+		
 	if $TileMap/player.position.x < $TileMap/NPC_Fishing_Leader.position.x:
 		$TileMap/NPC_Fishing_Leader/AnimatedSprite2D.flip_h = true
 	else:
 		$TileMap/NPC_Fishing_Leader/AnimatedSprite2D.flip_h = false
-		
-	if $TileMap/player.position.x < $TileMap/NPC_Fishing_1.position.x:
-		$TileMap/NPC_Fishing_1/AnimatedSprite2D.flip_h = true
-	else:
-		$TileMap/NPC_Fishing_1/AnimatedSprite2D.flip_h = false
-		
